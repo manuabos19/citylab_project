05b6c30f1d04caffb7ccf724d61f4b68f3dd509d
Manuel Abos <manuelabosmaria@gmail.com>
2026-09-17 12:10:57 +0000

    Creamos carpeta rviz para almacenar el archivo rviz y asi poder llamarlo desde el launch. Tambien creamos nueva rama que usaremos para conectarnos a un robot real con sus parametros

--- robot_patrol/launch/start_patrolling.launch.py
+++ robot_patrol/launch/start_patrolling.launch.py
@@ -1,11 +1,31 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
+
+    rviz_config = os.path.join(
+        get_package_share_directory('robot_patrol'),
+        'rviz',
+        'patrol.rviz'
+    )
+
     return LaunchDescription([
         Node(
             package='robot_patrol',
             executable='patrol_executable',
+            name='patrol',
             output='screen',
-            emulate_tty=True)
+            emulate_tty=True
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config]
+        ),
     ])
